91b5b75fac6e0befc7665f552a85772992e51628
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-12-20 12:47:52 -0600

    added delete button which is passed the handleDelete method from the main app file to delete records.

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Player = ({ player }) => {
+const Player = ({ player, handleDelete }) => {
     return(
         <div style={{border: '1px solid black'}}>
             <h3>{player.playerName.firstName ? player.playerName.firstName : 'Default'} {player.playerName.lastName ? player.playerName.lastName : 'Name'}</h3>
@@ -8,6 +8,7 @@ const Player = ({ player }) => {
                 <li>Wins: {player.wins}</li>
                 <li>Losses: {player.losses}</li>
             </ul>
+            <button type="submit" onClick={() => handleDelete(player.id)}>Delete</button>
         </div>
     )
 }
