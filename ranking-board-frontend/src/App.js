e6d32d6be3a387bd97cb9b74d900e7a6593e1c50
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2020-01-14 17:01:33 -0600

    Refactor app file on frontend to remove login functionality to separate component

--- ranking-board-frontend/src/App.js
+++ ranking-board-frontend/src/App.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import  Footer  from './components/Footer'
 import Game from './components/Game'
+import { GameContainer } from './components/Game/Game.styled'
 import gameService from './services/games'
+import loginService from './services/login'
+import { LoginForm, GameForm } from './components/Form'
 
 const App = () => {
   
   const [games, setGames] = useState([])
   const [newGame, setNewGame] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
 
+  // HOOK - GRABS ALL GAMES IN DB
   useEffect(() => {
     gameService
       .getAll()
       .then(initialGames => setGames(initialGames))
+
   }, [])
-  
-  const handleLogin = e => {
-    e.preventDefault()
-    console.log('Logging in with username', username, password)
+
+  // DISPLAY ALL GAMES
+  const displayGames = () => games.map(game => (
+    <Game key={game.id} {...game} />
+  ))
+
+  const displayUserGames = (user) => {
+
   }
 
   const handleGameChange = e => {
@@ -39,13 +46,55 @@ const App = () => {
       })
   }
 
+
+
   return (
-    <div>
-      <Game />
+    <div>    
+      
+      <LoginForm />
+
+      <GameForm />
+
+      <GameContainer>
+        {displayGames()}
+      </GameContainer>
+
+      {console.log(games)}
+
       <Footer />
+
     </div>
   )
 
 }
 
 export default App
+
+
+
+{/* 
+  {user === null ?
+    <h2>Login</h2> :
+    <h2>Welcome, {user.name}</h2>
+  }
+*/}
+
+{/*
+  {user === null ?
+    loginForm() : 
+    <div>
+      <p>{user.name} logged in</p>
+      <button onClick={handleLogOut}>Log Out</button>
+      {console.log('user info', user)}
+    </div>
+  }
+*/}
+
+{/*
+  {user !== null && gameForm()} 
+*/}
+
+{/* 
+  CAN MAKE CONDITIONAL
+  {user === null ? loginForm() : gameForm()}
+*/}
